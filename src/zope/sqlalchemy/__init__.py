46294c710e4b121213b10152f41c17bdecea05dd
Chris McDonough <chrism@plope.com>
2012-05-19 12:56:19 +0000

    back to dev

--- src/zope/sqlalchemy/__init__.py
+++ src/zope/sqlalchemy/__init__.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.7.1'
+__version__ = '0.7.2dev'
 
 from zope.sqlalchemy.datamanager import ZopeTransactionExtension, mark_changed
 invalidate = mark_changed
