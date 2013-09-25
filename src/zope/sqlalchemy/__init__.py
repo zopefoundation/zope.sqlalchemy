29c2f5c31ef19e11d373b6caba05e4c85aa8a02a
Laurence Rowe <laurence@lrowe.co.uk>
2013-09-25 13:33:05 -0700

    Prepare zope.sqlalchemy 0.7.3.

--- src/zope/sqlalchemy/__init__.py
+++ src/zope/sqlalchemy/__init__.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.7.3dev'
+__version__ = '0.7.3'
 
 from zope.sqlalchemy.datamanager import ZopeTransactionExtension, mark_changed
 invalidate = mark_changed
