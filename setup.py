9b70ff1cdbebb328ed287183112b696c1184ec61
Marius Gedminas <marius@gedmin.as>
2016-06-23 08:11:50 +0300

    Back to development: 0.7.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ tests_require = ['zope.testing']
 
 setup(
     name='zope.sqlalchemy',
-    version='0.7.7',
+    version='0.7.8.dev0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
