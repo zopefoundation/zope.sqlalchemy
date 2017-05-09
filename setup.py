c327c017af7424fb6aeb058b8a970f0f70915c39
Michael Howitz <mh@gocept.com>
2017-05-09 08:18:00 +0200

    PEP-8

--- setup.py
+++ setup.py
@@ -17,27 +17,29 @@ setup(
     author_email='laurence@lrowe.co.uk',
     url='http://pypi.python.org/pypi/zope.sqlalchemy',
     description="Minimal Zope/SQLAlchemy transaction integration",
-    long_description=open(os.path.join('src', 'zope', 'sqlalchemy', 'README.txt')).read() + "\n\n" +
-                     open('CHANGES.txt').read(),
+    long_description=(
+        open(os.path.join('src', 'zope', 'sqlalchemy', 'README.txt')).read() +
+        "\n\n" +
+        open('CHANGES.txt').read()),
     license='ZPL 2.1',
     keywords='zope zope3 sqlalchemy',
     classifiers=[
-    "Framework :: Zope3",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.3",
-    "License :: OSI Approved :: Zope Public License",
-    "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: Zope Public License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 
     install_requires=[
-      'setuptools',
-      'SQLAlchemy>=0.5.1',
-      'transaction < 1.5',
-      'zope.interface>=3.6.0',
-      ],
+        'setuptools',
+        'SQLAlchemy>=0.5.1',
+        'transaction < 1.5',
+        'zope.interface>=3.6.0',
+    ],
     extras_require={'test': tests_require},
     tests_require=tests_require,
-    )
+)
